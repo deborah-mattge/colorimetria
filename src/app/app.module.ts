f656b00871f58b1c28abaf2991b585a55f3a3fa6
anaclaraborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-06-14 19:38:48 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BotaoComponent } from './botao/botao.component';
+import { FormsModule } from '@angular/forms';
 import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 import { ContaComponent } from './conta/conta.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { CombinacoesComponent } from './combinacoes/combinacoes.component';
+import { AppRoutingModule } from './approuting.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BotaoComponent,
     TelaInicialComponent,
     ContaComponent,
     QuizComponent,
     CombinacoesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
