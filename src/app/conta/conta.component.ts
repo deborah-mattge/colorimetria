4904afb664f7950ff2c8c9c99df30858220e8706
heloisa_nascimento1 <helofogaca2015@gmail.com>
2023-06-14 21:52:17 -0300

    Implementa cadastro e login

--- src/app/conta/conta.component.ts
+++ src/app/conta/conta.component.ts
@@ -8,8 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class ContaComponent implements OnInit {
 
   constructor() { }
+  ngOnInit(){
+    const contasADD=localStorage.getItem("contas")
+    if(contasADD){
+      this.contas = JSON.parse(contasADD)
+    }
+  }
+  nome : string
+  contas: string[] = [];
 
-  ngOnInit(): void {
+  cadastrarNome(nome:string){
+    console.log(this.nome)
+    this.contas.push(this.nome);
+    console.log(this.contas)  
+    this.salvarLocalStorage()
+    this.nome='';
+    
+
+  }
+  currentPage: string = 'login';
+  
+  changePage(page: string) {
+    this.currentPage = page;
   }
 
+  valida(){
+    if(this.currentPage=='login'){
+      this.currentPage='cadastro';
+    } else if(this.currentPage=='cadastro'){
+      this.currentPage='login';
+    }
+  }
+
+  
+  salvarLocalStorage(){
+    localStorage.setItem("contas",JSON.stringify(this.contas))
+  }
 }
