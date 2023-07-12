79a684e2913bf71d35af37277830bc10e5b2e094
ANA CLARA TOMASELLI BORCHARDT <ana_c_borchardt@estudante.sesisenai.org.br>
2023-07-12 19:29:20 -0300

    igual a main

--- src/app/conta/conta.component.ts
+++ src/app/conta/conta.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { QuizComponent } from '../quiz/quiz.component';
-
 
 
 interface Conta {
@@ -20,20 +18,14 @@ interface Conta {
 })
 export class ContaComponent implements OnInit {
 
-  mensagemContaCadastrada: string = '';
   contaLogada: Conta;
   contaCadastrada: number;
-  nome: string;
   listaContas: Conta[] = [];
   generoFeminino = false;
   generoMasculino = false;
   cadastro: any = {};
   contaExistente: boolean = false;
   pagina: string = 'login';
-  resultadoQuiz: string | null = null;
-  paginaAtual: string = 'quiz';
-
-
 
   constructor(private router: Router, private authService: AuthService) { }
 
@@ -48,16 +40,21 @@ export class ContaComponent implements OnInit {
       this.pagina = 'cadastro';
     } else if (this.pagina == 'cadastro') {
       this.pagina = 'login';
+    } 
+  }
+
+  paginaLogada(){
+    if(this.contaCadastrada==1){
+      this.pagina = 'contaLogada';
     }
   }
 
-  trocarPagina(pagina: string) {
- this.paginaAtual = pagina;
-}
-
   cadastrar() {
     if (this.cadastro.email.indexOf("@") === -1 || this.cadastro.email.indexOf(".") === -1) {
-      alert('O email informado é inválido.');
+      alert('O email informado é inválido!');
+      this.cadastro = {};
+      this.generoFeminino = false;
+      this.generoMasculino = false;
       return;
     }
       const conta: Conta = {
@@ -72,23 +69,21 @@ export class ContaComponent implements OnInit {
  
     for (const c of this.listaContas) {
       if (c.email === conta.email) {
-        console.log("Conta já cadastrada");
         contaExistente = true;
         break;
       }
     }
- 
     if (contaExistente) {
-      alert("Esta conta já está cadastrada.");
+      alert("Esta conta já está cadastrada!");
     } else {
       this.listaContas.push(conta);
-      console.log(this.listaContas);
       this.salvarLocalStorage();
       this.contaCadastrada = 4;
       this.cadastro = {};
       this.generoFeminino = false;
       this.generoMasculino = false;
     }
+    this.authService.setContaCadastrada(false);
   }
 
   atualizarContaLogada() {
@@ -107,63 +102,38 @@ export class ContaComponent implements OnInit {
         }
       }
   
-      localStorage.setItem('contas', JSON.stringify(this.listaContas));
+      this.salvarLocalStorage(); 
     }
 }
 
-login() {
-  const emailLogin = this.cadastro.email;
-  const senhaLogin = this.cadastro.senha;
-  let contaEncontrada = false;
+  login() {
+    const emailLogin = this.cadastro.email;
+    const senhaLogin = this.cadastro.senha;
+    for (const conta of this.listaContas) {
+      if (conta.email === emailLogin && conta.senha === senhaLogin) {
+        this.contaCadastrada = 1;
+        this.contaLogada = conta; 
+        localStorage.setItem("Conta logada", JSON.stringify(this.contaLogada));
+        break;
+      } else {
+        this.contaCadastrada = 3;
+      }
+    }
+    localStorage.setItem("Número", JSON.stringify(this.contaCadastrada));
 
-  for (const conta of this.listaContas) {
-    if (conta.email === emailLogin && conta.senha === senhaLogin) {
-      this.contaCadastrada = 1;
+    if (this.contaCadastrada == 1) {
       this.authService.setContaCadastrada(true);
-      this.contaLogada = conta;
-      localStorage.setItem("Conta logada", JSON.stringify(this.contaLogada));
-      contaEncontrada = true;
-      break;
     } else {
-      this.contaCadastrada = 3;
+      alert("Email ou senha inválidos!")
+      this.authService.setContaCadastrada(false);
     }
+    this.cadastro.email='';
+    this.cadastro.senha='';
   }
-
-  localStorage.setItem("Número", JSON.stringify(this.contaCadastrada));
-
-  if (this.contaCadastrada === 1 && contaEncontrada) {
-    if (this.contaLogada.tipoPaleta === null) {
-      this.authService.setContaCadastrada(true);
-      this.router.navigate(['/quiz']);
-    } else if(this.contaLogada.tipoPaleta != null){
-      this.authService.setContaCadastrada(true);
-      this.trocarPagina('resultados');
-    }
-    
-    else {
-      this.mensagemContaCadastrada = 'Conta não cadastrada';
-    }
-  }
-
-  this.cadastro.email = '';
-  this.cadastro.senha = '';
-  console.log(this.valida);
-}
-
-
 
   salvarLocalStorage() {
     localStorage.setItem("contas", JSON.stringify(this.listaContas));
   }
-  logout(){
-    this.contaCadastrada=3;
-    this.authService.setContaCadastrada(false);
-  }
-
-
-
-
-
 }
 
 
